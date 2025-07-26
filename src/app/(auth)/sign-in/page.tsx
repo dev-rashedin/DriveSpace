e8452d25cde49f66be0618bf268c1c28e921f4f7
Rashedin-063 <rashedinislam.06@gmail.com>
2025-07-26 17:18:55 +0600

    auth form created

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,6 @@
+import AuthForm from "@/components/AuthForm"
+
 const SignIn = () => {
-  return (
-    <div> SignIn</div>
-  )
+  return <AuthForm type='sign-in'/>
 }
 export default SignIn
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,6 +1,6 @@
+import AuthForm from "@/components/AuthForm"
+
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
+  return <AuthForm type='sign-up' />
 }
 export default SignUp
