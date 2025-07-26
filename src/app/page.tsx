fa5f67755e70186848aaa6071d28217453945dda
Rashedin-063 <rashedinislam.06@gmail.com>
2025-07-26 10:44:53 +0600

    global css utility styles added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className=' min-h-screen flex flex-col justify-center items-center'>
+    <div className='flex-center min-h-screen'>
       <h1 className='text-3xl md:text-4xl lg:text-5xl font-semibold'>
         <span className='text-brand-200 font-black'> DriveSpace</span> -
         the only storage solution you need
