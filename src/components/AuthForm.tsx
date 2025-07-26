c5e06dd8f3b3e3ea86c606c5ff8c9a883a534b6d
Rashedin-063 <rashedinislam.06@gmail.com>
2025-07-26 22:59:41 +0600

    updated

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -16,6 +16,7 @@ import { Input } from '@/components/ui/input';
 import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { createAccount } from '@/lib/actions/user.actions';
 
 type FormType = 'sign-in' | 'sign-up';
 
@@ -36,6 +37,7 @@ const authFormSchema = (formType: FormType) => {
 const AuthForm = ({ type }: { type: FormType }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const [accountId, setAccountId] = useState('');
 
   const formSchema = authFormSchema(type);
 
@@ -49,7 +51,24 @@ const AuthForm = ({ type }: { type: FormType }) => {
   });
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    setIsLoading(true);
+    
+    const user = createAccount({
+      fullName: values.fullName  || '',
+      email: values.email,
+    })
+      .then((res) => {
+        console.log(res);
+        return res;
+      })
+      .catch((err) => {
+        console.log(err);
+        setErrorMessage(err.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+    };
   }
   return (
     <>
--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,8 +1,10 @@
 'use server';
 
-import { Query } from 'node-appwrite';
-import { createAdminClient } from '../appwrite';
+import { ID, Query } from 'node-appwrite';
+import { createAdminClient, createSessionClient } from '../appwrite';
 import { appwriteConfig } from '../appwrite/config';
+import { avatarPlaceholderUrl } from '@/app/constants';
+import { parseStringify } from '../utils';
 
 /**
  *** Create account flow
@@ -15,6 +17,7 @@ import { appwriteConfig } from '../appwrite/config';
  7. verify OTP and authenticate to login
  */
 
+//  get user by email
 const getUserByEmail = async (email: string) => {
   const { databases } = await createAdminClient();
 
@@ -27,6 +30,24 @@ const getUserByEmail = async (email: string) => {
   return result.total > 0 ? result.documents[0] : null;
 };
 
+const handleError = (error: unknown, message: string) => {
+  console.error(error, message);
+  throw error;
+};
+
+// sent email OTP
+const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createSessionClient();
+  
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+
+    return session.userId
+  } catch (error) {
+   handleError(error, 'Failed to send email OTP');
+  }
+}
+
 export const createAccount = async ({
   fullName,
   email,
@@ -35,4 +56,26 @@ export const createAccount = async ({
   email: string;
 }) => {
   const existingUser = await getUserByEmail(email);
+
+  const accountId = await sendEmailOTP({ email })
+  
+  if (!accountId) throw new Error('Failed to send OTP');
+
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
+
+    await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      ID.unique(),
+       {
+        fullName,
+        email,
+        avatar: avatarPlaceholderUrl,
+        accountId
+      }
+    )
+  }
+
+   return parseStringify({ accountId });
 };
