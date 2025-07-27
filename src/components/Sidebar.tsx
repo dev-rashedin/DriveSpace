adf8b8a1ae9426f68b51ea4dba0aa1ed230e59e2
Rashedin-063 <rashedinislam.06@gmail.com>
2025-07-27 21:48:59 +0600

    constants and sidebar updated, navbar added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,48 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link"
 import Logo from "./ui/logo";
+import { navItems } from "@/app/constants";
+import { usePathname } from "next/navigation";
+import { cn } from '@/lib/utils';
+
 
 
 const Sidebar = () => {
-  return (
-    <aside className='sidebar'>
-      {/* logo for sidebar */}   
-        <Logo type='sidebar' />
+    const pathname = usePathname();
 
+  return (
+    <aside className='sidebar bg-lime'>
+      {/* logo for sidebar */}
+      <Logo type='sidebar' />
+
+      <nav className='sidebar-nav'>
+        <ul className='flex flex-1 flex-col gap-6'>
+          {navItems.map(({ url, name, icon }) => (
+            <Link key={name} href={url} className='lg:w-full'>
+              <li
+                className={cn(
+                  'sidebar-nav-item',
+                  pathname === url && 'shad-active'
+                )}
+              >
+                <Image
+                  src={icon}
+                  alt={name}
+                  width={24}
+                  height={24}
+                  className={cn(
+                    'nav-icon',
+                    pathname === url && 'nav-icon-active'
+                  )}
+                />
+                <p className='hidden lg:block'>{name}</p>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </nav>
     </aside>
   );
 }
