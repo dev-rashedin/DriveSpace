ff07dd2d057009010b172e616fdc7d64cc53996d
Rashedin-063 <rashedinislam.06@gmail.com>
2025-07-31 00:35:39 +0600

    dynamic route updated and Sort component created

--- src/app/(root)/[type]/page.tsx
+++ src/app/(root)/[type]/page.tsx
@@ -1,8 +1,25 @@
 
 
-const page = () => {
+const page = async({ params }: SearchParamProps) => {
+  
+  const type = (await params)?.type as string
+
   return (
-    <div>page</div>
+    <div className="page-container">
+      <section className="w-full">
+        <h1 className='h1'>
+          {type}
+        </h1>
+        <div className="total-size-section">
+          <p className="body-1">
+            Total : <span>{ } 0 MB</span>
+          </p>
+          <div className="sort-container">
+            <p className="body-1 hidden text-light-200 sm:block">Sort by:</p>
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 export default page
