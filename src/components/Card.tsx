3d50abeea2d31bb924cb519c9a6a81d303e0375d
Rashedin-063 <rashedinislam.06@gmail.com>
2025-08-01 13:06:07 +0600

    convertFileSize utils added

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,27 @@
 import Link from 'next/link';
 import { Models } from 'node-appwrite';
 import Thumbnail from './Thumbnail';
+import { convertFileSize } from '@/lib/utils';
 
 const Card = ({ file }: { file: Models.Document }) => {
   return (
     <Link href={file.url} target='_blank' className='file-card'>
-      <div className="flex justify-between">
-        <Thumbnail type={file.type} extension={file.extension} url={file.url} className='!size-20' imageClassName='!size-11' />
+      <div className='flex justify-between'>
+        <Thumbnail
+          type={file.type}
+          extension={file.extension}
+          url={file.url}
+          className='!size-20'
+          imageClassName='!size-11'
+        />
       </div>
-    
-      <div className="flex flex-col items-end justify-between">
-        <p className='body-1'>{file.name}</p>
-        <p className='body-1'>{file.size}</p>
+
+      <div className='flex flex-col items-end justify-between'>
+      
+        <p className='body-1'>{convertFileSize(file.size)}</p>
       </div>
+
+    {file.name}
     </Link>
   );
 };
