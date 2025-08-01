f6f3493601ad8e9865fd705106f1519af2d8d864
Rashedin-063 <rashedinislam.06@gmail.com>
2025-08-01 16:06:42 +0600

    actionDropdown updated

--- src/components/ActionDropdown.tsx
+++ src/components/ActionDropdown.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
   Dialog,
 } from '@/components/ui/dialog';
@@ -10,19 +12,40 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import Image from 'next/image';
+import { useState } from 'react';
+import { Models } from 'node-appwrite';
+import { actionsDropdownItems } from '@/app/constants';
 
-const ActionDropdown = () => {
+const ActionDropdown = ({file} : {file: Models.Document}) => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+
   return (
-    <Dialog>
-      <DropdownMenu>
-        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+    <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+      <DropdownMenu open={isDropDownOpen} onOpenChange={setIsDropDownOpen}>
+        <DropdownMenuTrigger className='shad-no-focus cursor-pointer'>
+          <Image
+            src='/assets/icons/dots.svg'
+            alt='dots'
+            width={30}
+            height={30}
+          />
+        </DropdownMenuTrigger>
         <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuLabel className='max-w-[200px] truncate'>
+            {file.name}
+          </DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>Subscription</DropdownMenuItem>
+          {actionsDropdownItems.map((item) => 
+          (
+             <DropdownMenuItem key={item.value}>
+            {item.label}
+            </DropdownMenuItem>
+           )
+          )
+          }
         </DropdownMenuContent>
       </DropdownMenu>
     </Dialog>
