794d8d56ce6c37d777bc5b07cc24bd1cd8703a09
Rashedin-063 <rashedinislam.06@gmail.com>
2025-08-01 12:53:24 +0600

    card component updated

--- src/app/(root)/[type]/page.tsx
+++ src/app/(root)/[type]/page.tsx
@@ -29,7 +29,7 @@ const page = async({ params }: SearchParamProps) => {
 
       {files.total > 0 ? (
         <section className='file-list'>
-          {files.documents.map((file : Modals.Document) => (
+          {files.documents.map((file : Models.Document) => (
             <Card  key={file.$id} file={file}/>
           ))}
         </section>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,19 @@
+import Link from 'next/link';
+import { Models } from 'node-appwrite';
+import Thumbnail from './Thumbnail';
 
-
-const Card = () => {
+const Card = ({ file }: { file: Models.Document }) => {
   return (
-    <div>Card</div>
-  )
-}
-export default Card
+    <Link href={file.url} target='_blank' className='file-card'>
+      <div className="flex justify-between">
+        <Thumbnail type={file.type} extension={file.extension} url={file.url} className='!size-20' imageClassName='!size-11' />
+      </div>
+    
+      <div className="flex flex-col items-end justify-between">
+        <p className='body-1'>{file.name}</p>
+        <p className='body-1'>{file.size}</p>
+      </div>
+    </Link>
+  );
+};
+export default Card;
