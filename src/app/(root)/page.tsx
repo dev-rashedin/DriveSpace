9f71857b5e0c8caf016021b27d58627b9e986b78
Rashedin-063 <rashedinislam.06@gmail.com>
2025-08-04 20:55:55 +0600

    charts component created and mobileNavigation ui updated

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div className='flex-center min-h-screen'>
       <h1 className='h1'>
-        <span className='text-brand-200 font-black'> DriveSpace</span> -
+        <span className='font-black text-brand-200'> DriveSpace</span> -
         the only storage solution you need
       </h1>
     </div>
--- src/components/MobileNavigation.tsx
+++ src/components/MobileNavigation.tsx
@@ -33,90 +33,90 @@ const MobileNavigation = ({
   avatar,
   email,
 }: Props) => {
-
   const [open, setOpen] = useState(false);
-  const pathname = usePathname()
+  const pathname = usePathname();
 
   return (
+    <header className='mobile-header'>
+      <Logo type='navigation' />
 
-      <header className='mobile-header'>
-        <Logo type='navigation' />
-
-        <Sheet open={open} onOpenChange={setOpen}>
-          <SheetTrigger>
-            <Image
-              src='/assets/icons/menu.svg'
-              alt='Search'
-              width={30}
-              height={30}
-            />
-          </SheetTrigger>
-          <SheetContent className='shad-sheet h-screen px-3'>
-            <SheetTitle>
-              <div className='header-user'>
-                <Image
-                  src={avatar}
-                  alt='avatar'
-                  width={44}
-                  height={44}
-                  className='header-user-avatar'
-                />
-                <div className='sm:hidden lg:block'>
-                  <p className='subtitle-2 capitalize'>{fullName}</p>
-                  <p className='caption'>{email}</p>
-                </div>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger>
+          <Image
+            src='/assets/icons/menu.svg'
+            alt='Search'
+            width={30}
+            height={30}
+          />
+        </SheetTrigger>
+        <SheetContent className='sheet-drawer h-screen px-3'>
+          <SheetTitle>
+            <div className='header-user'>
+              <Image
+                src={avatar}
+                alt='avatar'
+                width={44}
+                height={44}
+                className='header-user-avatar'
+              />
+              <div className='sm:hidden lg:block'>
+                <p className='subtitle-2 capitalize'>{fullName}</p>
+                <p className='caption text-light-300'>{email}</p>
               </div>
-            </SheetTitle>
-
-            <Separator className='mb-4 h-px bg-light-200/50' />
-
-            {/* navigation */}
-            <nav className='mobile-nav'>
-              <ul className='mobile-nav-list'>
-                {navItems.map(({ url, name, icon }) => (
-                  <Link key={name} href={url} className='lg:w-full'>
-                    <li
-                      className={cn(
-                        'mobile-nav-item',
-                        pathname === url && 'shad-active'
-                      )}
-                    >
-                      <Image
-                        src={icon}
-                        alt={name}
-                        width={24}
-                        height={24}
-                        className='nav-icon'
-                      />
-                      <p className='body-1'>{name}</p>
-                    </li>
-                  </Link>
-                ))}
-              </ul>
-            </nav>
-
-            <Separator className='mb-4 h-px bg-light-200/50' />
-
-            {/* file uploader & logout button */}
-            <div className='flex flex-col justify-between gap-5 pb-5'>
-              <FileUploader ownerId={ownerId} accountId={accountId} />
-              <Button
-                type='submit'
-                className='mobile-sign-out-button'
-                onClick={async () => await signOutUser()}
-              >
-                <Image
-                  src='/assets/icons/logout.svg'
-                  alt='logo'
-                  width={24}
-                  height={24}
-                />
-                <p>Logout</p>
-              </Button>
             </div>
-          </SheetContent>
-        </Sheet>
-      </header>
+          </SheetTitle>
+
+          <Separator className='mb-4 h-px bg-light-300/50' />
+
+          {/* navigation */}
+          <nav className='mobile-nav'>
+            <ul className='mobile-nav-list'>
+              {navItems.map(({ url, name, icon }) => (
+                <Link key={name} href={url} className='lg:w-full'>
+                  <li
+                    className={cn(
+                      'mobile-nav-item',
+                      pathname === url && 'nav-active'
+                    )}
+                  >
+                    <Image
+                      src={icon}
+                      alt={name}
+                      width={24}
+                      height={24}
+                      className={cn(
+                        'nav-icon',
+                        pathname === url && 'nav-icon-active'
+                      )}
+                    />
+                    <p className='body-1'>{name}</p>
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </nav>
+
+          {/* file uploader & logout button */}
+          <Separator className='mb-4 h-px bg-light-300/50' />
+          <div className='flex flex-col justify-between gap-5 pb-5'>
+            <FileUploader ownerId={ownerId} accountId={accountId} />
+            <Button
+              type='submit'
+              className='mobile-sign-out-button'
+              onClick={async () => await signOutUser()}
+            >
+              <Image
+                src='/assets/icons/logout.svg'
+                alt='logo'
+                width={24}
+                height={24}
+              />
+              <p>Logout</p>
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </header>
   );
 };
 export default MobileNavigation;
