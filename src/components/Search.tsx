1afefc1444862ff19c879e925296e5b73d4977b0
Rashedin-063 <rashedinislam.06@gmail.com>
2025-08-04 15:49:07 +0600

    searched file displayed

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,11 +1,38 @@
 "use client"
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Input } from './ui/input';
+import { useSearchParams } from 'next/navigation';
+import { getFiles } from '@/lib/actions/file.actions';
+import { Models } from 'node-appwrite';
+import FormattedDateTime from './FormattedDateTime';
+import Thumbnail from './Thumbnail';
 
 const Search = () => {
-
+  const searchParams = useSearchParams()
   const [query, setQuery] = useState('');
+  const searchQuery = searchParams.get('query') || '';
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+
+
+  useEffect(() => {
+   const fetchFiles = async () => {
+     const files = await getFiles({ searchText: query });
+     setResults(files.documents);
+     setOpen(true)
+    }
+    
+    fetchFiles();
+  }, [searchQuery, query]);
+
+  // reset search query
+  useEffect(() => {
+    
+    if (!searchQuery) {
+    setQuery('');
+  }
+  }, [searchQuery]);
   
   return (
     <div className='search'>
@@ -16,7 +43,45 @@ const Search = () => {
           width={24}
           height={24}
         />
-        <Input value={query} placeholder='Search...' className='search-input' onChange={(e) => setQuery(e.target.value)} />
+        <Input
+          value={query}
+          placeholder='Search...'
+          className='search-input'
+          onChange={(e) => setQuery(e.target.value)}
+        />
+
+        {open && (
+          <ul className='search-result'>
+            {results.length > 0 ? (
+              results.map((file) => (
+                <li
+                  className='flex items-center justify-between'
+                  key={file.$id}
+                  // onClick={() => handleClickItem(file)}
+                >
+                  <div className='flex cursor-pointer items-center gap-4'>
+                    <Thumbnail
+                      type={file.type}
+                      extension={file.extension}
+                      url={file.url}
+                      className='size-9 min-w-9'
+                    />
+                    <p className='subtitle-2 line-clamp-1 text-light-100'>
+                      {file.name}
+                    </p>
+                  </div>
+
+                  <FormattedDateTime
+                    date={file.$createdAt}
+                    className='caption line-clamp-1 text-light-200'
+                  />
+                </li>
+              ))
+            ) : (
+              <p className='empty-result'>No files found</p>
+            )}
+          </ul>
+        )}
       </div>
     </div>
   );
